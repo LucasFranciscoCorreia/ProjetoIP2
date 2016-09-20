83c365eb1405082d6a010e5caea07551e1ba1432
Unknown <raissa_camelo@hotmail.com>
2016-09-20 20:57:15 -0300

    Raissa espera

---
+++ Projeto/src/feijao/Animal.java
@@ -0,0 +1,20 @@
+package feijao;
+
+public class Animal {
+
+	private boolean isAlive;
+	private String especie;
+	private String raca;
+	private String donoNome;
+	private String donoCPF;
+	private double peso;
+	private double tamanho;
+	
+	
+	public Animal(boolean isAlive, S){
+		
+		
+	}
+}
+
+
