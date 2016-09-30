72e0550cc9cb3120ef31f797c77dc0e89846c53b
Maria Fernanda <fsandrade25@gmail.com>
2016-09-30 13:02:06 -0300

    Alterações no Funcionario
    Mudança na classe teste, no repositorio  funcionario e na classe funcionario. Adicionei algumas variaveis de verificação e o padrào singleton.

--- Projeto/src/br/ufrpe/beans/Funcionario.java
+++ Projeto/src/br/ufrpe/beans/Funcionario.java
@@ -1,5 +1,7 @@
 package br.ufrpe.beans;
+
 import java.time.LocalDateTime;
+
 public class Funcionario {
 	private String nome;
 	private String cpf;
@@ -7,6 +9,7 @@ public class Funcionario {
 	private double salario;
 	private LocalDateTime entrada;
 	private String cargo;	
+	
 	public Funcionario(String nome, String cpf, Endereco endereco, 
 			double salario, LocalDateTime entrada, String cargo){
 		this.nome = nome;
@@ -16,42 +19,51 @@ public class Funcionario {
 		this.entrada = entrada;
 		this.cargo = cargo;
 	}	
+	
 	public String getCargo(){
 		return cargo;
 	}	
+	
 	public void setCargo(String cargo){
 		this.cargo = cargo;
 	}	
+	
 	public String getCPF(){
 		return cpf;
 	}	
+	
 	public String getNome(){
 		return nome;
 	}	
-	public void setNome(String nome){
-		this.nome = nome;
-	}	
+	
 	public void setEndereco(Endereco endereco){
 		this.endereco = endereco;
 	}	
+	
+	public Endereco getEnderecoS(){
+		return this.endereco;
+	}
+	
 	public String getEndereco(){
 		return this.endereco.toString();
 	}	
+	
 	public void setSalario(double salario){
 		this.salario = salario;
 	}	
+	
 	public double getSalario(){
 		return this.salario;
 	}	
-	public void setEntrada(LocalDateTime entrada){
-		this.entrada = entrada;
-	}	
+	
 	public String getEntrada(){
 		return String.format("%d/%d/%d %d:%d:%d", entrada.getYear(), entrada.getMonthValue(), entrada.getYear(), entrada.getHour(), entrada.getMinute(), entrada.getSecond());
 	}	
+	
 	public boolean equals(Funcionario outro){
 		return cpf.equals(outro.getCPF());
 	}	
+	
 	public String toString(){
 		return "Nome: " + this.nome + " - CPF: " + this.cpf + " - Cargo: " + this.cargo + 
 				" - Salario: " + this.salario + " - Endereco: " + this.endereco.toString(); 
--- Projeto/src/br/ufrpe/test/testFuncionario.java
+++ Projeto/src/br/ufrpe/test/testFuncionario.java
@@ -1,51 +1,48 @@
 package br.ufrpe.test;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import br.ufrpe.beans.Endereco;
 import br.ufrpe.beans.Funcionario;
 import br.ufrpe.repositorios.RepositorioFuncionario;
+
 public class testFuncionario {
 	public static void main(String[] args) {
 		Endereco end1 = new Endereco();
 		LocalDateTime entrada = LocalDateTime.now();
+		RepositorioFuncionario repositorio = RepositorioFuncionario.getInstanciado();
+		
 		Funcionario f1 = new Funcionario("Lucas", "101.575.184-93", end1, 3499f, entrada, "Balconista");
 		Funcionario f2 = new Funcionario("Lucas", "101.575.184-93", end1, 3499f, entrada, "Balconista");
-		System.out.println(f1);
+		Funcionario f3 = new Funcionario("Fernanda", "103.364.574-56", end1, 7845f, entrada, "Gerente");
+		Funcionario f4 = new Funcionario("Raissa", "563.642.624-78", end1, 8799f, entrada, "Recepcionista");
+		
+		System.out.println("************Equals************\n");
+		System.out.println("Equals f1 e f2: " + f1.equals(f2));
+		System.out.println("equals f1 e f3: " + f1.equals(f3));
+		
+		System.out.println("\n\n************Adicionar ao Repositorio************\n");
+		System.out.println("f1 adicionar = " + repositorio.cadastrar(f1));
+		System.out.println("f2 adicionar = " + repositorio.cadastrar(f2));
+		System.out.println("f3 adicionar = " + repositorio.cadastrar(f3));
+		System.out.println("f4 adicionar = " + repositorio.cadastrar(f4));
+		
+		System.out.println("\n\n************Atualização************\n");
+		
 		f1.setCargo("Veterinario");
-		f1.setEntrada(LocalDateTime.now());
-		f1.setNome("Jennifer");
 		f1.setSalario(8499f);
-		System.out.println();
-		System.out.println(f1);
-		System.out.println();
-		System.out.println(f1.equals(f2));
-		f2 = new Funcionario("Lucas", "101.575.184-94", end1, 3499f, entrada, "Balconista");
-		System.out.println(f1.equals(f2));
-		RepositorioFuncionario rep1 = new RepositorioFuncionario();
-		rep1.cadastrar(f1);
-		rep1.cadastrar(f2);
-		for (int i = 0; i < rep1.Size(); i++) {
-			System.out.println(rep1.buscar(i));
+		System.out.println("f1 atualizado = " + repositorio.atualizar(f1));
+		
+		System.out.println("\n\n************Imprimir todos os cadastrados************\n");
+		for (int i = 0; i < repositorio.Size(); i++) {
+			System.out.println(repositorio.buscar(i) + "\n");
 		}
-		rep1.remover(f1);
-		System.out.println();
-		for(int i = 0; i < rep1.Size(); i++){
-			System.out.println(rep1.buscar(i));
+		
+		System.out.println("\n\n************Remover************\n");
+		System.out.println("Remover f1 = " + repositorio.remover("101.575.184-93"));
+		
+		System.out.println("\n\n************Imprimir todos os cadastrados************\n");
+		for(int i = 0; i < repositorio.Size(); i++){
+			System.out.println(repositorio.buscar(i) + "\n");
 		}
-		Funcionario v[] = new Funcionario[2];
-		v[0] = f1;
-		v[1] = f2;
-		RepositorioFuncionario rep2 = new RepositorioFuncionario(v);
-		for (int i = 0; i < rep2.Size(); i++) {
-			System.out.println(rep2.buscar(i));
-		}
-		System.out.println();
-		rep2.remover(f1);
-		for (int i = 0; i < rep2.Size(); i++) {
-			System.out.println(rep2.buscar(i));
-		}
-		System.out.println();
 	}
 }
