bdf0283206cad26cee3eb319b638266b62e3d0ad
Maria Fernanda <fsandrade25@gmail.com>
2017-01-26 15:50:03 -0300

    testando o egit, criei uma classe de teste do egit.

---
+++ Projeto/src/br/ufrpe/beans/ClassedeTestdoEgit.java
@@ -0,0 +1,10 @@
+package br.ufrpe.beans;
+
+public class ClassedeTestdoEgit {
+
+	public static void main(String[] args) {
+		System.out.println("Testando o egit, não tem nd haver com o projeto");
+
+	}
+
+}
