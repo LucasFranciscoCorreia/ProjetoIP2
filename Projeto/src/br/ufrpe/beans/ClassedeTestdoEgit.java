302af278ffbd50d3790eafaa95122ba2df94a864
Maria Fernanda <fsandrade25@gmail.com>
2017-01-26 15:54:10 -0300

    Update ClassedeTestdoEgit.java

--- Projeto/src/br/ufrpe/beans/ClassedeTestdoEgit.java
+++ Projeto/src/br/ufrpe/beans/ClassedeTestdoEgit.java
@@ -3,7 +3,8 @@ package br.ufrpe.beans;
 public class ClassedeTestdoEgit {
 
 	public static void main(String[] args) {
-		System.out.println("Testando o egit, n„o tem nd haver com o projeto");
+		System.out.println("Testando o egit, n√£o tem nd haver com o projeto");
+		//modificando
 
 	}
 
