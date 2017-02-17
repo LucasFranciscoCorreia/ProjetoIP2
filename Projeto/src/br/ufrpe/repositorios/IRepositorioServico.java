1f7e19622452889aa1b4fbaf9611ecf582377d43
Srta Camelo <raissa_camelo@hotmail.com>
2017-02-17 12:06:12 -0300

    Repositorio Serviços

---
+++ Projeto/src/br/ufrpe/repositorios/IRepositorioServico.java
@@ -0,0 +1,20 @@
+package br.ufrpe.repositorios;
+import br.ufrpe.beans.*;
+
+public interface IRepositorioServico {
+	
+	/**
+	 * Interface do Repositório de Serviços
+	 * 
+	 * @author srtacamelo
+	 * @see Servico
+	 * @exception ObjetcJaExisteException
+	 * @exception ObjectNaoExisteException
+	 * @exception CodigoNaoExisteException
+	 * 
+	 */
+	
+	public IRepositorioServico{
+		
+	}
+}
