133d1c50de4fe252cfaab8b726620c94ffc1ec82
Maria Fernanda <fsandrade25@gmail.com>
2017-02-18 21:10:49 -0300

    merge

--- Projeto/src/br/ufrpe/repositorios/IRepositorioServico.java
+++ Projeto/src/br/ufrpe/repositorios/IRepositorioServico.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.ufrpe.repositorios;
 import br.ufrpe.beans.Servico;
 import br.ufrpe.excecoes.CodigoNaoExisteException;
@@ -26,32 +25,3 @@ public interface IRepositorioServico {
 	
 	
 }
-=======
-package br.ufrpe.repositorios;
-import br.ufrpe.beans.Servico;
-import br.ufrpe.excecoes.CodigoNaoExisteException;
-import br.ufrpe.excecoes.ObjectJaExisteException;
-import br.ufrpe.excecoes.ObjectNaoExisteException;
-
-/**
- * Interface do Repositório de Serviços
- * 
- * @author srtacamelo
- * @see Servico
- * @exception ObjetcJaExisteException
- * @exception ObjectNaoExisteException
- * @exception CodigoNaoExisteException
- * 
- */
-public interface IRepositorioServico {
-	
-	public void addAoRepositorio(Servico servico) throws ObjectNaoExisteException, ObjectJaExisteException;
-	public void removeDoRepositorio(Servico servico)throws ObjectNaoExisteException;
-	public void removerDoRepositorioNome(String nome);
-	public Servico pesquisarNomeNoRepositorioS(String nome) throws ObjectNaoExisteException;
-	public int pesquisarNomeNoRepositorioI(String nome)throws ObjectNaoExisteException;
-	
-	
-	
-}
->>>>>>> branch 'master' of https://github.com/LucasFranciscoCorreia/ProjetoIP2.git
