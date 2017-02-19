f4874584b321348d7f0f18ec2ed03736d10bf5bd
Srta Camelo <raissa_camelo@hotmail.com>
2017-02-19 18:11:17 -0300

    Controller Servico, att

--- Projeto/src/br/ufrpe/negocios/ControladorServico.java
+++ Projeto/src/br/ufrpe/negocios/ControladorServico.java
@@ -1,4 +1,8 @@
 package br.ufrpe.negocios;
+import br.ufrpe.beans.Servico;
+import br.ufrpe.excecoes.CodigoNaoExisteException;
+import br.ufrpe.excecoes.ObjectJaExisteException;
+import br.ufrpe.excecoes.ObjectNaoExisteException;
 import br.ufrpe.repositorios.*;
 public class ControladorServico {
 
@@ -6,5 +10,29 @@ public class ControladorServico {
 	
 	public ControladorServico(IRepositorioServico instance){
 		this.repositorioServico = instance;
+	}
+	void cadastrarServico(Servico novo)throws ObjectJaExisteException{
+		
+	}
+	void removerServico(Servico servico)throws CodigoNaoExisteException{
+		
+	}
+	void removerServicoNome(String nome)throws ObjectNaoExisteException{
+		
+	}
+	Servico buscarServico(String nome)throws ObjectNaoExisteException{
+		
+	}
+	int buscarNome(String nome)throws ObjectNaoExisteException{
+		
+	}
+	int buscarServico(Servico servico) throws ObjectNaoExisteException{
+		
+	}
+	void atualizarServico(Servico novo, Servico antigo)throws ObjectNaoExisteException{
+		
+	}
+	void salvarNoArquivo(){
+		
 	}
 }
