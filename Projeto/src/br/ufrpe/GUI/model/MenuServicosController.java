25c83842b65a9a3852bc4814c2460f32c2d325a5
Maria Fernanda <fsandrade25@gmail.com>
2017-02-19 23:59:44 -0300

    testando

--- Projeto/src/br/ufrpe/GUI/model/MenuServicosController.java
+++ Projeto/src/br/ufrpe/GUI/model/MenuServicosController.java
@@ -8,8 +8,10 @@ import br.ufrpe.negocios.FachadaControlador;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MenuServicosController {
@@ -21,6 +23,10 @@ public class MenuServicosController {
 	private TableColumn<Servico, String> codigoCol;
 	@FXML
 	private TableColumn<Servico, Float> precoCol;
+	@FXML
+	private TextField nome, preco;
+	@FXML
+	private Label aviso;
 	
 	public void preencherTabela(){
 		ArrayList<Servico> servicoListar = FachadaControlador.getInstance().listarServico();
@@ -30,6 +36,11 @@ public class MenuServicosController {
 		codigoCol.setCellValueFactory(new PropertyValueFactory<Servico, String>("codigo"));
 		
 		servicosTable.setItems(FXCollections.observableArrayList(servicoListar));
+	}
+	
+	@FXML
+	public void adicionarServico(ActionEvent evt){
+		
 	}
 	
 	@FXML
