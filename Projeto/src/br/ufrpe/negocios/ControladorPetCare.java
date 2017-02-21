e1d0fc9b2854b04c1a36d488d04331873287a1c8
Maria Fernanda <fsandrade25@gmail.com>
2017-02-21 03:27:03 -0300

    começo de implementação de ServiçoAndamento e ServiçoConcluido.

--- Projeto/src/br/ufrpe/negocios/ControladorPetCare.java
+++ Projeto/src/br/ufrpe/negocios/ControladorPetCare.java
@@ -1,11 +1,13 @@
 package br.ufrpe.negocios;
 
+import java.util.ArrayList;
+
 import br.ufrpe.beans.PetCare;
 import br.ufrpe.excecoes.ObjectJaExisteException;
 import br.ufrpe.excecoes.ObjectNaoExisteException;
 import br.ufrpe.repositorios.IRepositorioPetCare;
 
-public class ControladorPetCare {
+public class ControladorPetCare implements IControladorPetCare{
 
 	private IRepositorioPetCare repositorioPetCare;
 	
@@ -21,5 +23,13 @@ public class ControladorPetCare {
 	}
 	public void salvarNoArquivo(){
 		this.repositorioPetCare.salvarNoArquivo();
+	}
+	
+	public ArrayList<PetCare> listarServicoEmAndamento() {
+		return repositorioPetCare.listarServicoEmAndamento();
+	}
+	
+	public ArrayList<PetCare> listarServicoConcluido() {
+		return repositorioPetCare.listarServicoConcluido();
 	}
 }
